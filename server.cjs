deddf354ccb7eae6f9d75f3d9dcbaece849cb052
Robert Duke <robduke123@gmail.com>
2023-12-17 14:55:44 +1300

    finished the adding events functionality

--- server.cjs
+++ server.cjs
@@ -18,7 +18,7 @@ const db = knex({
   }
 });
 
-app.get('/', (req,res) => {
+app.get('/', (res) => {
     res.json('it is working!')
 })
 
@@ -36,6 +36,9 @@ app.post('/signin', (req, res) => {
             .where('email', '=', email)
             .then(user => {
                 res.json(user[0])
+                // console.log(user[0].event_dates[0].slice(0,15));
+                // console.log(user[0].event_dates[0].slice(17,32));
+                // console.log(user[0].event_dates[0].length);
             })
             .catch(err => res.status(400).json('unable to get user'))
         } else {
@@ -64,7 +67,19 @@ app.post('/register', (req, res) => {
                 .insert({
                     first_name: firstName,
                     last_name: lastName,
-                    email: loginEmail[0].email,                    
+                    email: loginEmail[0].email, 
+                    event_name: [],
+                    event_details: [],
+                    event_dates: [],
+                    event_time: [],
+                    event_period: [],
+                    event_sun: [],
+                    event_mon: [],
+                    event_tue: [],
+                    event_wed: [],
+                    event_thu: [],
+                    event_fri: [],
+                    event_sat: []                   
                 })
                 .then(user => {
                     res.json(user[0])
@@ -74,6 +89,32 @@ app.post('/register', (req, res) => {
         .catch(trx.rollback)
     })
     .catch(err => res.status(400).json('unable to register'))
+})
+
+app.put('/add', (req, res) => {
+    const {email, name, details, dates, time, period, Sun, Mon, Tue, Wed, Thu, Fri, Sat} = req.body
+
+    db.select('*').from('users').where({email: email})    
+    .returning('*')
+    .then(data => {
+        db.select('*').from('users').where({email: email})
+        .update({
+            event_name: [...data[0].event_name, name],
+            event_details: [...data[0].event_details, details],
+            event_dates: [...data[0].event_dates, dates],
+            event_time: [...data[0].event_time, time],
+            event_period: [...data[0].event_period, period],
+            event_sun: [...data[0].event_sun, Sun],
+            event_mon: [...data[0].event_mon, Mon],
+            event_tue: [...data[0].event_tue, Tue],
+            event_wed: [...data[0].event_wed, Wed],
+            event_thu: [...data[0].event_thu, Thu],
+            event_fri: [...data[0].event_fri, Fri],
+            event_sat: [...data[0].event_sat, Sat]
+        })
+        .returning('*')
+        .then(data => res.json(data[0]))
+    })
 })
 
 app.listen(3000, () => {
